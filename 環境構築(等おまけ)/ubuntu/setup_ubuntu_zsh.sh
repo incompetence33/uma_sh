dd89ec710b89067997350e0e6bdd82b8e8d28a87
incompetence33 <sukasukamiyoune@gmail.com>
2022-05-06 22:20:57 +0900

    ubuntu 22.04では /bin/zsh がなかったため /usr/bin/zsh に変更

--- 環境構築(等おまけ)/ubuntu/setup_ubuntu_zsh.sh
+++ 環境構築(等おまけ)/ubuntu/setup_ubuntu_zsh.sh
@@ -25,7 +25,7 @@ rm ~/zsh_setup_rcs.zip
 echo "デフォルトのShellをBashからZshにします。"
 echo "パスワードを入力してください。"
 sudo sed '$a /bin/zsh' /etc/shells
-chsh -s /bin/zsh
+chsh -s /usr/bin/zsh
 git clone https://github.com/zsh-users/zsh-completions.git ~/.zsh-completions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh-syntax-highlighting
 sudo update-locale LANG="ja_JP.UTF-8"
