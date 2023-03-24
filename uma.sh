4cbd633d0d4a0ab103d443b4397150472be374b0
incompetence33 <sukasukamiyoune@gmail.com>
2023-03-24 21:08:58 +0900

    vgmstreamのインストール修正

--- uma.sh
+++ uma.sh
@@ -82,12 +82,12 @@ if [[ "${DIST_FLAG}" == 0 ]]; then
 		sudo apt upgrade -y
 		sudo apt install tar git sqlite3 curl 
 		mkdir -p ~/commands/bin
-		if curl -o ~/vgmstream-cli.tar.gz $(curl -s https://dl.vgmstream.org | tr '"' '\n' | \grep 'linux/vgmstream-linux-cli.tar.gz'); then
-			tar -xvf ~/vgmstream-linux-cli.tar.gz -C ~/commands/bin
+		if curl -Lo ~/vgmstream-cli.tar.gz $(curl -sL http://vgmstream.org/ | tr '"' '\n' | \grep 'vgmstream-linux-cli.tar.gz'); then
+			tar -xvf ~/vgmstream-cli.tar.gz -C ~/commands/bin
 			rm ~/vgmstream-cli.tar.gz
 		else
 			curl -Lo ~/vgmstream-cli.zip "https://github.com/vgmstream/vgmstream/releases/download/$(curl -sL 'https://github.com/vgmstream/vgmstream/releases/latest' |grep \<title\>|awk '{print $2}')/vgmstream-cli-linux.zip"
-			unzip -d ~/commands/bin ~/vgmstream-cli-linux.zip
+			unzip -d ~/commands/bin ~/vgmstream-cli.zip
 			rm ~/vgmstream-cli.zip
 		fi
 		chmod 777 ~/commands/bin/vgmstream-cli
